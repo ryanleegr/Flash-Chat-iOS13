716ff6b180e1c4065e2a3e0d26c209266dae806a
Ryan Lee <ryan.leegr@gmail.com>
2022-04-21 14:30:00 -0400

    Fini

---
+++ Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by ryan lee on 3/22/22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String //Email of person who sent the message
+    let body: String 
+}
